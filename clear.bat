86f62d24bc5360a7e30ec9274512a1154045663c
Nour Hasan <nh_bappy@outlook.com>
2020-06-20 20:20:39 +0600

    Bug Fixed
    Initial Blank Page

---
+++ clear.bat
@@ -0,0 +1,9 @@
+php artisan clear-compiled
+php artisan view:clear
+php artisan config:cache
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan debugbar:clear
+
+PAUSE
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -156,7 +156,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <img src="{{ asset('img/profile.png') }}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">Admin</a>
+                    <a href="#" class="d-block">
+                        {{ Auth::user()->name ?? 'User' }}
+                    </a>
                 </div>
             </div>
 
